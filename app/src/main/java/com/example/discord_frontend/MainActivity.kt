c011149cf336c862e0c862cb19148c9f7aa97262
elaus00 <elaus00@gmail.com>
2024-08-19 17:47:03 +0900

    Github integrated to Discord Testing

--- app/src/main/java/com/example/discord_frontend/MainActivity.kt
+++ app/src/main/java/com/example/discord_frontend/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DiscordfrontendTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android testing",
+                        name = "Android testing Github Testing",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
